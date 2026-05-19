c23dbc7578e754f02b2fba81b71ff99abe51f0dc
Luiz Felipe <luizfelipe20004@gmail.com>
2026-05-18 22:23:17 -0300

    Correção formulario

--- script.js
+++ script.js
@@ -2,41 +2,13 @@
 
 const form = document.getElementById("contact-form");
 
-form.addEventListener("submit", function(event) {
-
-  // Campos
-  const nome = document.getElementById("nome").value;
-  const email = document.getElementById("email").value;
-  const mensagem = document.getElementById("mensagem").value;
+form.addEventListener("submit", function() {
 
   const feedback = document.getElementById("feedback");
 
-  // Regex simples para validar e-mail
-  const emailValido = /\S+@\S+\.\S+/;
-
-  // Verificação
-  if(nome === "" || email === "" || mensagem === "") {
-
-    feedback.innerText = "Preencha todos os campos.";
-
-    return;
-  }
-
-  if(!emailValido.test(email)) {
-
-    feedback.innerText = "Digite um e-mail válido.";
-
-    return;
-  }
-
-  // Mensagem sucesso
   feedback.innerText = "Mensagem enviada com sucesso!";
 
-  // Limpa formulário
-  form.reset();
-
 });
-
 
 // ===== TEMA ESCURO =====
 
